3b21df77db325a8dbeadcd670abfb1de00964931
Anjana Ambika <hianjana@gmail.com>
2026-05-05 09:08:49 +0530

    Delete NLP Internship directory

--- NLP Internship/shell script/crawl_spiders.sh
+++
@@ -1,9 +0,0 @@
-cd /home/ubuntu/alamode
-scrapy crawl lefashion
-scrapy crawl thebluehydrangeas
-scrapy crawl dressedinIbiza
-scrapy crawl fashiontrends
-scrapy crawl songofstyle
-scrapy crawl elle
-scrapy crawl whowhatwear
-echo 'All blogs crawled sucessfully!!!'
